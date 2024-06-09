5ee4f8e7833bbe98bc1641949418f12623e0916b
William Huang <whlxgx@gmail.com>
2024-06-09 16:55:16 -0700

    Adding color to daily changes according to stock price increasing/decreasing

--- src/Components/StockCard.js
+++ src/Components/StockCard.js
@@ -3,10 +3,17 @@ import "../Component CSS/StockCard.css"
 import { finnhubClient} from "../finnhubService"
 
 export default function StockCard(props) {
+    var style = {};
+    if (props.data) {
+        if (props.data.d !== 0) {
+            style = {
+                color: props.data.d > 0 ? "#81c995" : "#f28b82"
+            }
+        } else {
+            style = {color: "white"}
+        }
+    }
 
-    useEffect(() => {
-        
-    }, [])
     
 
     return (
@@ -14,7 +21,7 @@ export default function StockCard(props) {
             <h1 className="cardSymbol">{props.symbol}</h1>
             <div className="cardStats">
                 <h2>{props.data ? props.data.c : ""}</h2>
-                <h3>{props.data ? props.data.d : ""}</h3>
+                <h3 style={style}>{props.data ? props.data.d : ""}</h3>
             </div>
         </div>
     )
