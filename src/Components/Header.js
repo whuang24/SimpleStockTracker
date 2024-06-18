e20b2fa02341a5905b01488dc7eec0be1fa8bc6e
William Huang <whlxgx@gmail.com>
2024-06-18 01:14:12 -0700

    Added more styling changes to the headers, made it more visually easy to read.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,10 @@
 import React, {useEffect, useState} from "react"
 import "../Component CSS/Header.css"
-import { finnhubClient } from "../finnhubService";
+import { finnhubClient, isMarketOpen } from "../finnhubService";
 
 export default function Header(props) {
     const [stockBasics, setStockBasics] = useState({})
+    const [marketStatus, setMarketStatus] = useState(false);
 
     function removing(event, symbol) {
         event.stopPropagation();
@@ -17,17 +18,6 @@ export default function Header(props) {
             }
             
         })
-        
-        finnhubClient.quote(props.symbol, (error, data, response) => {
-            setStockBasics(oldData => {
-                return {
-                    ...oldData,
-                    currPrice: data.c,
-                    currChange: data.d,
-                }
-            })
-        })
-
         finnhubClient.companyProfile2({'symbol' : props.symbol}, (error, data, response) => {
             setStockBasics(oldData => {
                 return {
@@ -37,21 +27,59 @@ export default function Header(props) {
                 }
             })
         })
+
+        async function fetchData() {
+            finnhubClient.quote(props.symbol, (error, data, response) => {
+                setStockBasics(oldData => {
+                    var change = data.d > 0 ? `+${data.d}` : data.d;
+                    return {
+                        ...oldData,
+                        currPrice: data.c,
+                        currChange: change,
+                    }
+                })
+            })
+        }
+
+        async function checkMarket() {
+            const marketStatus = await isMarketOpen();
+            setMarketStatus(marketStatus);
+        }
+
+        checkMarket();
+        console.log(marketStatus);
+
+        fetchData();
+
+        if (marketStatus) {
+            const intervalId = setInterval(fetchData, 15000);
+            return () => clearInterval(intervalId);
+        }
+
     }, [props.symbol])
 
-    console.log(stockBasics);
-
+    var style = {};
+    if (stockBasics.currChange) {
+        if (stockBasics.currChange !== 0) {
+            style = {
+                color: stockBasics.currChange > 0 ? "#81c995" : "#f28b82"
+            }
+        } else {
+            style = {color: "white"}
+        }
+    }
 
     return (
         <nav className="header">
             <div className="leftHeader">
                 {<img className="companyLogo" src={stockBasics.logo} alt={`Logo of ${stockBasics.name}`}/>}
-                <h1 className="detailName">{stockBasics.name}</h1>
+                <h1 className="detailSymbol">{stockBasics.symbol}</h1>
+                <h2 className="detailName">{stockBasics.name}</h2>
                 <button onClick={(event) => removing(event, stockBasics.symbol)}>Unsubscribe</button>
             </div>
             <div className="rightHeader">
                 <h2 className="detailPrice">{stockBasics.currPrice}</h2>
-                <h3 className="detailChange">{stockBasics.currChange}</h3>
+                <h3 className="detailChange" style={style}>{stockBasics.currChange}</h3>
             </div>
         </nav>
     )
